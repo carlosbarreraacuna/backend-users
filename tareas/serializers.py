71ffdb87542a9e046e438184989f295747b1467b
unknown <carlos.barreraacuna@gmail.com>
2025-03-09 16:36:19 -0500

    ajuste serializers

--- tareas/serializers.py
+++ tareas/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Tarea
 
 class TareaSerializer(serializers.ModelSerializer):
+    persona_tipo_documento = serializers.CharField(source='persona.tipo_documento', read_only=True)
+    persona_numero_documento = serializers.CharField(source='persona.numero_documento', read_only=True)
     class Meta:
         model = Tarea
         fields = '__all__'
